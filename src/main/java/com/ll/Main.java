41894c431e41e1499779d1784f41b9981eb62bb7
gaeun220 <gaeun2701@gmail.com>
2024-11-27 15:37:22 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,15 +9,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         lab1();
-
+        lab2();
     }
 
     private static void lab1() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("명령) ");
         String cmd = scanner.nextLine().trim();
-        System.out.println("입력한 명령 : " + cmd);
 
+        System.out.println("입력한 명령 : " + cmd);
+    }
+
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("명령".getBytes());
+
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령 : " + cmd);
     }
 
 }
