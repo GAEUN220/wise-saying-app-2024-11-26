f9b610798f9cce2c8af41994c6ffdc4cf1867ec3
gaeun220 <gaeun2701@gmail.com>
2024-11-27 17:34:51 +0900

    work

---
+++ src/test/java/com/ll/AppTest.java
@@ -0,0 +1,27 @@
+package com.ll;
+
+import com.ll.App.App;
+import com.ll.standard.util.TestUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    public static String run(String input) {
+        input = input.stripIndent().trim();
+        Scanner scanner = TestUtil.getScanner(input);
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        return output;
+    }
+
+}
