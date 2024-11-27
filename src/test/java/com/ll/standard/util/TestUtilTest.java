ab8e1ef54bf0e1124ec97320ae4f63d7cf0c3b47
gaeun220 <gaeun2701@gmail.com>
2024-11-27 16:46:15 +0900

    work

--- src/test/java/com/ll/standard/util/TestUtilTest.java
+++ src/test/java/com/ll/standard/util/TestUtilTest.java
@@ -24,7 +24,7 @@ public class TestUtilTest {
 
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("나의 죽음을 적들에게 알라지 말라!");
-        assertThat(author).isEqualTo("이순신")
+        assertThat(author).isEqualTo("이순신");
 
     }
 
