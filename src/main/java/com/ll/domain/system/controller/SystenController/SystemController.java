b45296e14335f3a47cee35f8a21e6162d46b1eae
gaeun220 <gaeun2701@gmail.com>
2024-11-27 17:52:33 +0900

    work

---
+++ src/main/java/com/ll/domain/system/controller/SystenController/SystemController.java
@@ -0,0 +1,9 @@
+package com.ll.domain.system.controller.SystenController;
+
+public class SystemController {
+
+    public void actionExit() {
+        System.out.println("명언 앱을 종료합니다.");
+    }
+
+}
