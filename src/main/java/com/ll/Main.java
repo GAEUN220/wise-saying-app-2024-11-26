9182887c513520228a0828004406d5d5eea8c5d2
gaeun220 <gaeun2701@gmail.com>
2024-11-27 15:41:44 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        //lab1();
         lab2();
     }
 
@@ -21,14 +21,19 @@ public class Main {
     }
 
     private static void lab2() {
-        InputStream in = new ByteArrayInputStream("명령".getBytes());
-
+        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
         Scanner scanner = new Scanner(in);
 
         System.out.println("명령) ");
         String cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령 : " + cmd);
+
+        System.out.println("명령) ");
+        cmd = scanner.nextLine();
+
+        System.out.println("입력한 명령 : " + cmd);
+
     }
 
 }
