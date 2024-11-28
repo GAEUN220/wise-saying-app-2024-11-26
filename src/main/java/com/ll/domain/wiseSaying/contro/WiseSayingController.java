d389a95beccc6439b803232c78e4149a7fcb43c1
gaeun220 <gaeun2701@gmail.com>
2024-11-28 12:40:16 +0900

    level 3 GREEN

--- src/main/java/com/ll/domain/wiseSaying/contro/WiseSayingController.java
+++ src/main/java/com/ll/domain/wiseSaying/contro/WiseSayingController.java
@@ -11,6 +11,8 @@ public class WiseSayingController {
     public void actionAdd() {
         System.out.print("명언 : ");
         System.out.print("작가 : ");
+
+        System.out.print("1번 명언이 등록되었습니다.");
     }
 
 }
