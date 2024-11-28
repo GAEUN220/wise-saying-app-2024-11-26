baf508f3151aefeb0324c0e1d19703960c9a6112
gaeun220 <gaeun2701@gmail.com>
2024-11-28 14:27:50 +0900

    level 5 GREEN

--- src/main/java/com/ll/domain/wiseSaying/contro/WiseSayingController.java
+++ src/main/java/com/ll/domain/wiseSaying/contro/WiseSayingController.java
@@ -1,21 +1,32 @@
 package com.ll.domain.wiseSaying.contro;
 
+import com.ll.domain.wiseSaying.entity.WiseSaying;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingController {
     private final Scanner scanner;
     private int lastID;
+    private List<WiseSaying> wiseSayings;
 
     public WiseSayingController(Scanner scanner) {
         this.scanner = scanner;
         this.lastID = 0;
+        this.wiseSayings = new ArrayList<>();
     }
 
     public void actionAdd() {
         System.out.print("명언 : ");
+        String content = scanner.nextLine();
         System.out.print("작가 : ");
+        String author = scanner.nextLine();
 
         int id = ++lastID;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
 
         System.out.print(id +"번 명언이 등록되었습니다.");
 
@@ -26,8 +37,10 @@ public class WiseSayingController {
 
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
-        System.out.println("2 / 작자미상 / 과거에 집착하지 마라.");
-        System.out.println("1 / 작자미상 / 현재를 사랑하라.");
+
+        for (WiseSaying wiseSaying : wiseSayings.reversed()) {
+            System.out.println(wiseSaying.getId() + " / " + wiseSaying.getAuthor() + " / " + wiseSaying.getContent());
+        }
 
     }
 }
