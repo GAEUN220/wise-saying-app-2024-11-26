1714ec15954a6c8b0d610b7e9a95741c81a0dea7
gaeun220 <gaeun2701@gmail.com>
2024-11-28 17:38:04 +0900

    GREEN 2

--- src/main/java/com/ll/Command.java
+++ src/main/java/com/ll/Command.java
@@ -1,11 +1,25 @@
 package com.ll;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Command {
     private final String actionName;
+    private final Map<String, String> params;
 
     public Command(String cmd) {
         String[] cmdBits = cmd.trim().split("\\?");
         this.actionName = cmdBits[0];
+        this.params = new HashMap<>();
+
+        String queryString = cmdBits[1];
+
+        String[] params = queryString.split("&");
+
+        for (String param : params) {
+            String[] paramBits = param.split("=", 2);
+            this.params.put(paramBits[0], paramBits[1]);
+        }
     }
 
     public String getActionName() {
@@ -13,4 +27,7 @@ public class Command {
     }
 
 
+    public String getParam(String key) {
+        return params.get(key);
+    }
 }
