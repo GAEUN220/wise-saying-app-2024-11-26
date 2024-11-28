b209960a57b57cc28e6b8e432abbbbebc3aba4b5
gaeun220 <gaeun2701@gmail.com>
2024-11-28 12:46:24 +0900

    level4 GREEN

--- src/main/java/com/ll/domain/wiseSaying/contro/WiseSayingController.java
+++ src/main/java/com/ll/domain/wiseSaying/contro/WiseSayingController.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class WiseSayingController {
     private final Scanner scanner;
+    private int lastID;
+
     public WiseSayingController(Scanner scanner) {
         this.scanner = scanner;
+        this.lastID = 0;
     }
 
     public void actionAdd() {
         System.out.print("명언 : ");
         System.out.print("작가 : ");
 
-        System.out.print("1번 명언이 등록되었습니다.");
+        int id = ++lastID;
+
+        System.out.print(id +"번 명언이 등록되었습니다.");
+
+
     }
 
 }
