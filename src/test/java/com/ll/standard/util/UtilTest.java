024e383cff1a74dc0d9f4e34b0babcbdcfd5c3ec
gaeun220 <gaeun2701@gmail.com>
2024-12-05 17:27:25 +0900

    work

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -1,11 +1,23 @@
 package com.ll.standard.util;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilTest {
+    @BeforeAll
+    public static void beforeAll() {
+        Util.file.mkdir("temp");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        Util.file.rmdir("temp");
+    }
+
     @Test
     @DisplayName("파일을 생성할 수 있다.")
     public void t1() {
@@ -22,6 +34,7 @@ public class UtilTest {
 
         Util.file.delete(filePath);
     }
+
     @Test
     @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
     public void t2() {
@@ -54,5 +67,4 @@ public class UtilTest {
                 Util.file.notExists(filePath)
         ).isTrue();
     }
-
 }
