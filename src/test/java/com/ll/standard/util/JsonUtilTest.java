65787603fe4ede3e5c84f0c9f5b25f165a032dec
gaeun220 <gaeun2701@gmail.com>
2024-12-10 19:54:37 +0900

    f3 r

--- src/test/java/com/ll/standard/util/JsonUtilTest.java
+++ src/test/java/com/ll/standard/util/JsonUtilTest.java
@@ -45,4 +45,24 @@ public class JsonUtilTest {
             """.stripIndent().trim());
     }
 
+    @Test
+    @DisplayName("맵을 Json으로 바꿀 수 있다. (숫자 필드)")
+    public void t3() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("id", 1);
+        map.put("name", "이름");
+        map.put("gender", "남자");
+
+
+        String jsonStr = Util.json.toString(map);
+
+        assertThat(jsonStr).isEqualTo("""
+            {
+                "id" : 1,
+                "name" : "이름",
+                "gender" : "남자"
+            }
+            """.stripIndent().trim());
+    }
+
 }
